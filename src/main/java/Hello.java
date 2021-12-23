c58d6ec9998dda2e77b773203a8f7d6d50f51fd8
barutcuzafer <barutcuzafer1@gmail.com>
2021-12-23 08:16:59 +0300

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,12 @@
+
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing Hello Feature");
+        System.out.println("Githu Repo is already published to github");
+        System.out.println("Doing more work");
+
     }
 
 }
